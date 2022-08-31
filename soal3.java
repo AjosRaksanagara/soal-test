513125e618630e3c95b4111d8ed2f1f1789cabc3
AjosRaksanagara <112487051+AjosRaksanagara@users.noreply.github.com>
2022-08-31 09:39:57 +0700

    soal3.java
    Write function to find Balanced Brackets. Bracket is considered to be any one of the following
    characters: ( , ) , { , } , [ , or ]. Check brackets matched pairs between opening bracket and close
    bracket with return string YES or NO.

---
+++ soal3.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class BalancedBrackets {
+
+	{
+		Deque<Character> stack
+			= new ArrayDeque<Character>();
+
+	
+		for (int i = 0; i < expr.length(); i++) {
+			char x = expr.charAt(i);
+
+			if (x == '(' || x == '[' || x == '{') {
+				// Push the element in the stack
+				stack.push(x);
+				continue;
+			}
+
+	
+			if (stack.isEmpty())
+				return false;
+			char check;
+			switch (x) {
+			case ')':
+				check = stack.pop();
+				if (check == '{' || check == '[')
+					return false;
+				break;
+
+			case '}':
+				check = stack.pop();
+				if (check == '(' || check == '[')
+					return false;
+				break;
+
+			case ']':
+				check = stack.pop();
+				if (check == '(' || check == '{')
+					return false;
+				break;
+			}
+		}
+
+	
+		return (stack.isEmpty());
+	}
+
+	
+	public static void main(String[] args)
+	{
+		String expr = "([{}])";
+
+
+		if (areBracketsBalanced(expr))
+			System.out.println("YES ");
+		else
+			System.out.println("NO ");
+	}
+}
